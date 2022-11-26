4888258f7c24d212c8224ff9629a9a8aea1f9bdc
aviulsler <trashfuertrash@gmail.com>
2022-11-26 17:42:03 +0100

    Clean up + added js, css, audio and images folder

---
+++ js/Functions.js
@@ -0,0 +1,6 @@
+let play = document.getElementById("play");
+    function playMusic() {
+    let audio = new Audio("audio/Thomas.mp3");
+    audio.play()
+    }
+play.addEventListener("click", playMusic);
